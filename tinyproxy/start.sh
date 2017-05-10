4faa32c960bb49b9cf60b69fb06165600ccba276
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-05-09 21:36:35 -0300

    tinyproxy: support running container with read-only
    Suggest running as read-only, with writable pieces available as a tmpfs,
    for a reproducible and secure environment.
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- tinyproxy/start.sh
+++ tinyproxy/start.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-service tinyproxy start
+# Make sure the writable pieces are created and available before running the service
+mkdir -p /var/run/tinyproxy
+mkdir -p /var/log/tinyproxy
+touch /var/log/tinyproxy/tinyproxy.log
+chown -R tinyproxy:tinyproxy /var/log/tinyproxy /var/run/tinyproxy
+
+/usr/sbin/tinyproxy -c /etc/tinyproxy/tinyproxy.conf
 
 # Execute all the rest
 exec "$@"
