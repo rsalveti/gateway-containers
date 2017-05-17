376bcd9bca25154db444ab2742c18b91a955a1d5
Ricardo Salveti <ricardo.salveti@linaro.org>
2017-05-17 19:07:16 -0300

    bt-joiner: run bluetooth_6lowpand via docker CMD
    Remove from the entryscript start.sh, as then docker can restart the
    container in case of issues with the script.
    
    Signed-off-by: Ricardo Salveti <ricardo.salveti@linaro.org>

--- bt-joiner/start.sh
+++ bt-joiner/start.sh
@@ -9,7 +9,5 @@ mkdir -p /var/run/dbus
 
 mount -t debugfs none /sys/kernel/debug
 
-/usr/bin/bluetooth_6lowpand --loglevel 4 --daemonize --skip_init > /var/log/bluetooth_6lowpand.log 2>&1 &
-
 # Execute all the rest
 exec "$@"
